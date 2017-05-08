c186c9647703c3342be03c7390aa13578f0fbef8
村上恒平 <mikan.zuki319@gmail.com>
2017-05-08 16:31:16 +0900

    add

--- ナナクラフト/Game/Game/Map/mati.h
+++ ナナクラフト/Game/Game/Map/mati.h
@@ -1,30 +1,24 @@
 ﻿//kyoukai
 {
 	"kyoukai",
-	CVector3(-79.40f, 0.00f, 75.90f),             //平行移動
+	CVector3(-13.70f, 0.00f, 9.92f),             //平行移動
 	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
 },
 //house
 {
 	"house",
-	CVector3(65.00f, 0.00f, 85.40f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
-},
-//house
-{
-	"house",
-	CVector3(65.00f, 0.00f, 29.50f),             //平行移動
-	CQuaternion(0.00f, 0.71f, 0.71f, 0.00f ),  //回転
-},
-//house
-{
-	"house",
-	CVector3(104.70f, 0.00f, 85.40f),             //平行移動
+	CVector3(14.80f, 0.00f, 12.15f),             //平行移動
 	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
 },
 //mati
 {
 	"mati",
 	CVector3(0.00f, 0.00f, 0.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+},
+//ie
+{
+	"ie",
+	CVector3(-10.19f, 0.00f, -15.00f),             //平行移動
 	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
 },
